1dba91fa7e239eb231877d1aba48e4435d487b53
Antônio Sá Barreto <acsabarreto@gmail.com>
2019-06-14 00:21:58 -0300

    Fixing app in run.

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
-api = Api(app)
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.secret_key = 'jose'
+api = Api(application)
 
 
-jwt = JWT(app, authenticate, identity)  # /auth
+jwt = JWT(application, authenticate, identity)  # /auth
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Store, '/store/<string:name>')
@@ -24,5 +24,5 @@ api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
     from db import db
-    db.init_app(app)
+    db.init_app(application)
     app.run(port=5000, debug=True)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import app as application
+from app import application
 from db import db
 
 db.init_app(application)
